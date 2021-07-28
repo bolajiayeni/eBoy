12a3d4e0b221779283189b43438ceb9fc8e19ca1
bolajiayeni <ayenibolaji3@gmail.com>
2021-07-28 21:13:47 +0100

    Corrected some errors on HomeScreen code

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
-import data from "../../data";
-import Product from "../Product";
+import Product from "../components/Product";
+import data from "../data";
 
 export default function HomeScreen() {
   return (
     <div className="row center">
       {data.products.map((product) => {
-        return <Product key={product.id} product={product}></Product>;
+        return <Product key={product._id} product={product}></Product>;
       })}
     </div>
   );
