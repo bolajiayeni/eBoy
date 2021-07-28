36d7accab3cf7133b89714d26653b813b7d1d893
bolajiayeni <ayenibolaji3@gmail.com>
2021-07-28 20:10:40 +0100

    Refactored App.js for Home and product page routes with react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,36 @@
 import React from "react";
-import data from "./data";
-import Product from "./components/Product";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./components/screens/HomeScreen";
+import ProductScreen from "./components/screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div className="logo-box">
-          <a href="/" className="header-link logo-link">
-            eBoy!
-          </a>
-        </div>
-        <div className="nav-link">
-          <a href="/cart" className="header-link cart-link">
-            Cart
-          </a>
-          <a href="/signin" className="header-link signin-link">
-            Sign In
-          </a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => {
-            return <Product key={product.id} product={product}></Product>;
-          })}
-        </div>
-      </main>
-      <footer className="row center">
-        All Rights reserved &copy; Bolaji Ayeni
-      </footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div className="logo-box">
+            <a href="/" className="header-link logo-link">
+              eBoy!
+            </a>
+          </div>
+          <div className="nav-link">
+            <a href="/cart" className="header-link cart-link">
+              Cart
+            </a>
+            <a href="/signin" className="header-link signin-link">
+              Sign In
+            </a>
+          </div>
+        </header>
+        <main>
+          <Route path="/products/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">
+          All Rights reserved &copy; Bolaji Ayeni
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
