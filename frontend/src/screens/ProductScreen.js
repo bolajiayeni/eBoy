cbe92b4f47dba67dde8b359dabf9bf668e4c01e6
bolajiayeni <ayenibolaji3@gmail.com>
2021-07-29 21:04:38 +0100

    built ProductScreen structure

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Rating from "../components/Rating";
 import data from "../data";
 
 export default function ProductScreen({ match }) {
@@ -15,11 +16,34 @@ export default function ProductScreen({ match }) {
           <div className="product-page-route">
             Home/{product.category}/{product.name}
           </div>
-          <div className="product-page-title"></div>
-          <div className="product-page-description"></div>
+          <div className="product-page-title">{product.name}</div>
+          <div className="product-page-price">$ {product.price}</div>
+          <div className="product-page-description">{product.description}</div>
         </div>
-        <div className="col-1"></div>
-        <div className="col-1"></div>
+        <div className="col-1">
+          <img
+            className="product-page-img"
+            src={product.image}
+            alt={product.name}
+          />
+        </div>
+        <div className="col-1">
+          <Rating
+            rating={product.rating}
+            numOfReviews={product.numOfReview}
+          ></Rating>
+          <input
+            type="button"
+            value="Add to cart"
+            className="product-buy-btn"
+          />
+          <div className="product-page-status">
+            <div className="span">Status: </div>
+            <div className="span">
+              {product.price > 100 ? "Expensive" : "Cheap"}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
