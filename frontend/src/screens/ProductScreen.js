570162936da2bbe714efdb9d339a36101dd682b0
bolajiayeni <ayenibolaji3@gmail.com>
2021-08-02 21:56:17 +0100

    Refactored Product screen code

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
 import data from "../data";
 
@@ -11,36 +12,42 @@ export default function ProductScreen({ match }) {
     return <div className="row center">Product not found</div>;
   } else {
     return (
-      <div className="row">
-        <div className="col-1">
-          <div className="product-page-route">
-            Home/{product.category}/{product.name}
-          </div>
+      <div className="f-flex">
+        <div className="col-1 product-page-col">
+          <Link to="/" className="product-back-route">
+            back to results
+          </Link>
           <div className="product-page-title">{product.name}</div>
-          <div className="product-page-price">$ {product.price}</div>
-          <div className="product-page-description">{product.description}</div>
+          <div className="product-page-price">$ {product.price}.00</div>
+          <div className="product-page-description">
+            " {product.description} "
+          </div>
         </div>
-        <div className="col-1">
+        <div className="col-1 product-page-col">
           <img
             className="product-page-img"
             src={product.image}
             alt={product.name}
           />
         </div>
-        <div className="col-1">
-          <Rating
-            rating={product.rating}
-            numOfReviews={product.numOfReview}
-          ></Rating>
-          <input
-            type="button"
-            value="Add to cart"
-            className="product-buy-btn"
-          />
-          <div className="product-page-status">
-            <div className="span">Status: </div>
-            <div className="span">
-              {product.price > 100 ? "Expensive" : "Cheap"}
+        <div className="col-1 product-page-col product-cta-block">
+          <div className="cta">
+            <Rating
+              rating={product.rating}
+              numOfReviews={product.numOfReview}
+            ></Rating>
+            <input
+              type="button"
+              value="Add to cart"
+              className="product-buy-btn"
+            />
+            <div className="product-page-status">
+              <div className="">Status: </div>
+              <div
+                className={product.price > 100 ? "available" : "unavailable"}
+              >
+                {product.price > 100 ? "available" : "unavailable"}
+              </div>
             </div>
           </div>
         </div>
