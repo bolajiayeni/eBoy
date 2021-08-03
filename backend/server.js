0cd17ed110fb7c83f7707c9da729a52474843362
bolajiayeni <ayenibolaji3@gmail.com>
2021-08-03 21:13:13 +0100

    "Added api get route for products"

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ SERVER.JS : SIMPLE EXPRESS SERVER TO SERVE UP PRODUCT DATA
 
 // importing modules
 import express from "express";
+import data from "./data.js";
 
 //Initializing a new express application
 const app = express();
@@ -16,7 +17,11 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-//
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+//app.listen
 app.listen(port, () => {
   console.log("Server dey run g");
 });
