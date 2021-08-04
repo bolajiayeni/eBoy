46719777f5ce80f6640cb086b1f8ae0542741401
bolajiayeni <ayenibolaji3@gmail.com>
2021-08-04 21:43:31 +0100

    Added state to import data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "../components/Product";
-import data from "../data";
 
 export default function HomeScreen() {
+  //using useState hooks to set state for product array
+  const [products, setProducts] = useState([]);
+
+  //using useEffect to  import product array from backend
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="row center">
-      {data.products.map((product) => {
+      {/* rendering a product for each object in the product array using the map function */}
+      {products.map((product) => {
         return <Product key={product._id} product={product}></Product>;
       })}
     </div>
