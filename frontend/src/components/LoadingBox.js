dafa513d292aac314db4bad5dc1d9f384e6b98c3
bolajiayeni <ayenibolaji3@gmail.com>
2021-08-07 10:44:45 +0100

    Styled Loading box component

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoadingBox() {
   return (
-    <div className="full-center">
+    <div className="row">
       <div className="loader7"></div>
     </div>
   );
