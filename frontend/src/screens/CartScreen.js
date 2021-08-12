8f9e4343e3235907b22de5c89756f2ac3ca6deec
bolajiayeni <ayenibolaji3@gmail.com>
2021-08-12 23:59:01 +0100

    added cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { addToCart } from "../actions/cartActions";
+import MessageBox from "../components/MessageBox";
 
 export default function CartScreen(props) {
   const productId = props.match.params.id;
   const qty = props.location.search
     ? Number(props.location.search.split("=")[1])
     : 1;
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,11 +22,22 @@ export default function CartScreen(props) {
   }, [dispatch, productId, qty]);
 
   return (
-    <div>
-      <h1>Cart Screen</h1>
-      <p>
-        ADD TO CART : productId: {productId} Qty: {qty}
-      </p>
+    <div className="row">
+      <div className="cart-item-box">
+        <h1 className="cart-title">Shopping cart</h1>
+        {cartItems.length === 0 ? (
+          <MessageBox>
+            Cart is empty
+            <Link to="/">Go shopping</Link>
+          </MessageBox>
+        ) : (
+          <ul className="cart-list">
+            {cartItems.map((item) => {
+              <li className="cart-list-item" key={item.product}></li>;
+            })}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
